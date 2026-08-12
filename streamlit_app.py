23b083efcf61fdc4e56b32278e642c5f765cae88
Ved Kamath <vedastic25@gmail.com>
2026-08-11 17:37:34 -0700

    Change DRIVE_FILE_ID for model access
    Updated the Google Drive file ID for the model.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.write("Upload an image of a white blood cell to classify and save it.")
 
 MODEL_PATH = "wbc_model.h5"
 
-DRIVE_FILE_ID = "1HorMh00ZxmilZ9JGs9YyR1MNjUAewEFE"
+https://drive.google.com/file/d/1bI1Q3bVkW5HpbLmRONz6wK8uDR4ukvId/view?usp=drive_link
+DRIVE_FILE_ID = "1bI1Q3bVkW5HpbLmRONz6wK8uDR4ukvId"
 DRIVE_LINK = f"https://drive.google.com/uc?id={DRIVE_FILE_ID}"
 
 
